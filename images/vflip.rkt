599670f85c310d817e1576392509b78899cd8a4c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-02-01 09:21:08 -0600

    Minor cleanup to vflip code.

--- images/vflip.rkt
+++ images/vflip.rkt
@@ -7,8 +7,8 @@
 #|
 (define img1 (above (solid-square 30 "black") (outlined-square 24 "black" 3)))
 img1
-(hflip img1)
+(vflip img1)
 (define img2 (rotate (solid-rectangle 40 20 "red") 15))
 img2
-(hflip img2)
+(vflip img2)
 |#
